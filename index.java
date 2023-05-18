7161b3f568ca8e5c2e531a7c1bd21725be82b7fa
Brian <brian.aguinsaca@unl.edu.ec>
2023-05-17 20:44:14 -0500

    Agregar presentacion

--- index.java
+++ index.java
@@ -1,5 +1,6 @@
 public class Carpetita {
     public static void main(String[] args) {
         System.out.println("Hola, este es mi primer programa");
+        System.out.println("UNIVERSIDAD NACIONAL DE LOJA");
     }
 }
