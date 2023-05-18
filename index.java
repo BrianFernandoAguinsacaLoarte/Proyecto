043775885ae00f9fa9d92aff31a134b75102ddd3
Darwin Andrés Sarango Soto <132624867+DarwinSarango@users.noreply.github.com>
2023-05-17 21:08:01 -0500

    Actualización de la clase

--- index.java
+++ index.java
@@ -1,9 +1,11 @@
-public class Carpetita {
+public class Futbol{
     public static void main(String[] args) {
-        System.out.println("Hola, este es mi primer programa");
+        System.out.println("Fútbol");
+        System.out.prinln("Número de Participantes");
+        System.out.prinln("Duración del Juego");
+        System.out.prinln("Número de Participantes")
+        System.out.println("Estadisticas Generales")
         System.out.println("UNIVERSIDAD NACIONAL DE LOJA");
     }
 }
-public class Carpeta{
-    System.out.println("Crear una rama")
-}
+
