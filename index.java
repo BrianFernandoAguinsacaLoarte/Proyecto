eeb7c3416c066e8a14af9519f00759996b05f61b
Abel214 <131829158+Abel214@users.noreply.github.com>
2023-05-17 20:51:12 -0500

    Prueba  ocmentario

--- index.java
+++ index.java
@@ -2,5 +2,6 @@ public class Carpetita {
     public static void main(String[] args) {
         System.out.println("Hola, este es mi primer programa");
         System.out.println("UNIVERSIDAD NACIONAL DE LOJA");
+        System.out.println("BUENOS DIAS");
     }
 }
